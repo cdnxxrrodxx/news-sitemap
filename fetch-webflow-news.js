901f8b7178ae3a84f5094014cecb42afe5563047
Matt Lawrence <matt_lawrence@digitaldynasty.ca>
2025-05-27 17:20:22 -0400

    Update fetch-webflow-news.js

--- fetch-webflow-news.js
+++ fetch-webflow-news.js
@@ -9,16 +9,16 @@ const SITE_BASE_URL = "https://www.heavyweightboxing.com";
 (async () => {
   const response = await fetch(`https://api.webflow.com/collections/${COLLECTION_ID}/items?limit=100`, {
     headers: {
-      Authorization: `Bearer ${WEBFLOW_API_TOKEN}`,
-      "accept-version": "1.0.0",
+      "Authorization": `Bearer ${WEBFLOW_API_TOKEN}`,
+      "accept-version": "1.0.0"
     }
   });
 
   //const { items } = await response.json();
-  const resJson = await response.json();
-console.log(JSON.stringify(resJson, null, 2));
+console.log("Status Code:", response.status);
+const resJson = await response.json();
+console.log("Response:", JSON.stringify(resJson, null, 2));
 
-const { items } = resJson;
 //
 
   const now = Date.now();
